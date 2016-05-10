53b689ad4793aed0e6a002853d3656a04a87472e
adhamehab <adhaamehab7@gmail.com>
2016-05-10 05:50:04 -0700

    First Edition
    TODO: Test special cases
    : Explanation

---
+++ HuffmanCompression/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HuffmanCompression
+{
+    static class Program
+    {
+       
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            /*
+                to DO : 
+                    - Fix Decompressor
+                    - add dirctory choose to (ZIPPEd/UNZIPPED) File 
+                    - add new Features(Types)
+
+            
+            */
+        }
+    }
+}
